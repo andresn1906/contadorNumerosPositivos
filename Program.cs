c8c197854cf4e867b6ce4c0158b3cf1558323579
Andrés Suárez <andresuarez1906@gmail.com>
2025-07-23 16:06:55 -0500

    feat: :wrench: Contador números positivos

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 0;
+        int contNum = 0;
+        int sum = 0;
+
+        Console.WriteLine("Ingrese números enteros positivos.\n Ingrese un número negativo para finalizar.");
+
+        while (true)
+        {
+            Console.Write("Número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 0)
+            {
+                contNum = contNum + 1;
+                sum = sum + num;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"Se ha ingresado un número negativo {num}.\n Finalizando el programa...");
+                Console.WriteLine($"Cantidad de números: {contNum}\nSuma de números: {sum}\n");
+                break;
+            }
+        }
+    }
+}    
